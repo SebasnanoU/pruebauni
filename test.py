cf68893272f40c2f4bbe943894bdff682a6760f6
sebasnanou <jsrodriguezp03@libertadores.edu.co>
2022-10-17 19:16:52 -0500

    se corrijen los tests para que funcionen con pytets

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-import unittest
-
 from sum_nuemros import sum_number
 
-class TestSumNumbers(unittest.TestCase):
+def test_sum_numbers_default():
+    """Test that the default value is a list"""
+    assert sum_number() == 5050
+    assert sum_number(numbers=None) == 5050
     
-    def test_sum_numbers_default(self):
-        """Test that the default value is a list"""
-        self.assertEqual(sum_number(), 5050)
-        self.assertEqual(sum_number(numbers=None), 5050)
-        
 
-    def test_sum_numbers_with_list(self):
-        self.assertEqual(sum_number([1, 2, 3]), 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_sum_numbers_with_list():
+    assert sum_number([1, 2, 3]) == 6
